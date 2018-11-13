daa56b093e7a626646151dc012a40a10bf531c40
Guilherme <ggpasqualino@gmail.com>
2018-11-13 20:12:20 +0100

    Add attr_writer, reader, accessor, object interaction

--- lesson11/classes.rb
+++ lesson11/classes.rb
@@ -1,18 +1,53 @@
 class Person
+  attr_reader :name
+  # attr_writer :age
+  attr_accessor :age
 
   # Constructor
   def initialize(name)
-    @name = "My name is #{name}"
+    @age = 0
+    @name = name
   end
 
   # Getter method
-  def name
-    @name
+  # def name
+  #   @name
+  # end
+
+  # Getter method
+  # def age
+  #   @age
+  # end
+
+  # Setter method
+  # def age=(age)
+  #   @age = age
+  # end
+
+  def get_older
+    @age = @age + 1
+  end
+
+  def greet_me
+    if @age == 1
+      "Hello, I am #{@name}. I am #{@age} year old"
+    else
+      "Hello, I am #{@name}. I am #{@age} years old"
+    end
+  end
+
+  def greet(other_person)
+    "Hi, #{other_person.name}! I am #{@name}."
   end
 end
 
-calculator1 = Person.new("Fernando")
-calculator2 = Person.new("Jill")
+person1 = Person.new("Fernando")
+person2 = Person.new("Jill")
 
-puts calculator1.name
-puts calculator2.name
+puts person1.name
+puts person1.greet_me
+person1.get_older
+puts person1.greet_me
+person1.age = 20
+puts person1.greet_me
+puts person1.greet(person2)
